6093d6f35aca11d8f0329f2affbfd4b414552ac9
nosremetnarg <grant.alex.emerson@gmail.com>
2020-08-11 11:32:48 -0500

    linking pages

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -92,6 +93,21 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+
+
 
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
