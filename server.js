679eaa8b8680061526af763ded086996ac2fa437
nosremetnarg <grant.alex.emerson@gmail.com>
2020-08-11 09:08:40 -0500

    another bug

--- server.js
+++ server.js
@@ -97,16 +97,15 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 err back
+    // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted');
-    }
-    else {
-        // add animal to json file and animals array in this function
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
 });
+
 
 
 app.listen(PORT, () => {
